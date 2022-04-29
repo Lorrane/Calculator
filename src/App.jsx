51ba45dfc49e33788ede2aa44b7a1b15b038226e
Lorrane Machado <lorranemachado21@gmail.com>
2022-04-29 14:51:14 -0300

    ajuste nos botoes

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,87 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
+    const [display, setDisplay] = useState("");
 
-  const [display, setDisplay] = useState("");
-  
-  const keyboard = [
-    "7", "8", "9", "/", "C",
-    "4", "5", "6", "*", "Del",
-    "1", "2", "3", "-", "Ins",
-    "0", ".", "+", "="
-  ];
+    const keyboard = [
+        "7",
+        "8",
+        "9",
+        "/",
+        "C",
+        "4",
+        "5",
+        "6",
+        "*",
+        "Del",
+        "1",
+        "2",
+        "3",
+        "-",
+        "Ins",
+        "0",
+        ".",
+        "+",
+        "=",
+    ];
 
-  const handleClick = (button) => {
-     if (button === "="){
-      setDisplay(eval(display));
-    } else {
-      setDisplay(`${display}${button}`)
-    }
-    }
-  
+    const handleClick = (button) => {
+        switch (button) {
+            case "=":
+                setDisplay(eval(display));
+                break;
 
-  return (
-    <div className="container">
-      <header>
-        <h1>Calculadora</h1>
-        <p>Criada usando React</p>
-      </header>
+            case "C":
+                setDisplay("");
+                break;
 
+            case "Del":
+                setDisplay(display.substring(0, display.length - 1));
+                break;
 
-      <main>
-        {/* aqui ficará todo o código da calculadora */}
-        <input type="text" value={display} className="display" />
-        <div className="buttons">
-          {keyboard.map(button  => (
-            <Button button={button} onClick={handleClick} key={button} className={button === "0" && "span2"}>{button}</Button>
-          ))}
+            default:
+                setDisplay(`${display}${button}`);
+        }
+    };
+
+    return (
+        <div className="container">
+            <header>
+                <h1>Calculadora</h1>
+                <p>Criada usando React</p>
+            </header>
+
+            <main>
+                {/* aqui ficará todo o código da calculadora */}
+                <input 
+                    type="text" 
+                    value={display} 
+                    onChange = {(event) => {
+                      setDisplay(event.target.value)
+                    }}
+                    className="display" 
+                />
+                <div className="buttons">
+                    {keyboard.map((button) => {
+                        const span2Class = button === "0" ? "span2" : "";
+                        const primaryClass = isNaN(button) ? "primary" : "";
+                        return (
+                            <Button
+                                button={button}
+                                onClick={handleClick}
+                                key={button}
+                                className={`${span2Class} ${primaryClass}`}
+                            >
+                                {button}
+                            </Button>
+                        );
+                    })}
+                </div>
+            </main>
+
+            <footer>Feita por Lorrane</footer>
         </div>
-      </main>
-
-      <footer>Feita por Lorrane</footer>
-    </div>
-  );
+    );
 }
 
 export default App;
